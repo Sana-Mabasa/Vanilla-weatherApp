f24e05ed4c04b9e20672e2acf807250f71b7aaca
Sana-Mabasa <mabasasana59@gmail.com>
2023-06-17 07:41:46 +0200

    updated time

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+function formatDate(timestamp) {
+  // calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temp");
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
@@ -10,12 +34,12 @@ function displayTemp(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = Math.round(response.data.main.humidity);
+  let dateElement = document.querySelector("#day");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = `866a208a73eeff02182218e9441647a1`;
-let city = `Tokyo`;
+let city = `Pretoria`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemp);
